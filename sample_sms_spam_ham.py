18a5190ef5289af173633ba029b430862d1c5c55
FrontMage <xbgxwh@outlook.com>
2019-07-15 10:49:38 +0800

    add sample data

---
+++ sample_sms_spam_ham.py
@@ -0,0 +1,24 @@
+import pandas as pd
+df = pd.read_csv('./sms_spam_ham.csv')
+df.columns = ["label", "text"]
+print(df.head(10))
+
+neg_count = 0
+pos_count = 0
+max_pos_count = 75478
+train = []
+for _,row in df.iterrows():
+    if row["label"] == 1:
+        neg_count+=1
+        if neg_count>max_pos_count:
+            continue
+    else:
+        pos_count+=1
+        if pos_count>max_pos_count:
+            continue
+    train.append([row["label"], row["text"]])
+
+new_df = pd.DataFrame(train)
+
+print(len(new_df))
+new_df.to_csv('./sms_spam_ham_sample.csv')
